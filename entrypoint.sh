52c93809ce57f99a1a37bf0d2260650d100a6f6a
zen <hasee>
2024-12-23 19:52:58 +0800

    瑟瑟是第一生产力

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,19 @@
 #!/bin/ash
 
+# 读取 authentication 文件
+AUTH_FILE="/data/authentication"
+
+# 检查文件是否存在
+if [[ ! -f "$AUTH_FILE" ]]; then
+    echo "Authentication file not found!"
+    exit 1
+fi
+
+# 读取 BDUSS 和 STOKEN
+
+# shellcheck disable=SC1090
+source "$AUTH_FILE"
+
 # 检查 BDUSS 和 STOKEN 是否存在
 if [[ -z "$BDUSS" && -z "$STOKEN" ]]; then
     echo "Neither BDUSS nor STOKEN is set!"
